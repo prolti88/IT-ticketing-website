b219954113177e1c43f1156dd8dac18830e98125
prolti88 <169029930+prolti88@users.noreply.github.com>
2024-05-11 12:34:38 +0200

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,23 @@
+<?php 
+$ablakcim = array('cim'=>'AT IT Services');
+
+$fejlec = array('kepforras'=>'logo.png','kepalt'=>'logo','cim'=>'IT Services Helpdesk Portal');
+
+$lablec = array('copyright'=>'Copyright '.date("Y").'.','owner'=>'- IT Services');
+
+$oldalak = array(
+    '/' =>array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+    'bejelentkezes' => array('fajl' => 'bejelentkezes', 'szoveg' => 'Bejelentkezés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kijelentkezés', 'menun' => array(0,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztracio' => array('fajl' => 'regisztracio', 'szoveg' => '', 'menun' => array(0,0)),
+    'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek' , 'menun' => array(1,1)),
+    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'hibajegy' => array('fajl' => 'hibajegy', 'szoveg' => 'Hibajegy', 'menun' => array(1,1)),
+    'hibajegyek' => array('fajl' => 'hibajegyek', 'szoveg' => 'Hibajegyek', 'menun' => array(0,1)),
+    );
+
+$hiba_oldal = array('fajl'=>'404','szoveg'=>'A keresett oldal nem található!','menun' => array(1,1));
+
+?>
+
